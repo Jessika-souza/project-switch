088c29765d0816cdd6be15479fd3707120994f46
Jessika Souza <jessikauk@outlook.com>
2024-06-06 09:21:45 +0100

    first push

---
+++ scripit.js
@@ -0,0 +1,26 @@
+function toggleMode() {
+  const html = document.documentElement; //pegando html
+  html.classList.toggle("light"); //troca das classes
+
+  // if(html.classList.contains('light')){
+  //   html.classList.remove('light')
+  //} else{
+  //    html.classList.add('light')
+  //}
+
+  //pegar a tag img
+
+  const img = document.querySelector("#profile img");
+
+  // substituir a imagem
+
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute("alt", "Imagem light");
+  } else {
+    // se tiver sem light mode, mannter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute("alt", "Imagem escura");
+  }
+}
